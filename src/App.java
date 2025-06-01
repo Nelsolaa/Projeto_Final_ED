85c2550551bd0bfe662b5da6a889d392e1734ee2
Nelson Neto <111544659+Nelsolaa@users.noreply.github.com>
2025-06-01 00:24:43 -0300

    first commit

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
 
     public static void main(String[] args) {
-        String arquivoPalavrasChave = "C:\\Users\\nelso\\OneDrive\\Área de Trabalho\\javafx\\Trabalho_Final_Estrutura\\src\\palavrasChaves.txt";
-        String arquivoTexto = "C:\\Users\\nelso\\OneDrive\\Área de Trabalho\\javafx\\Trabalho_Final_Estrutura\\src\\textoInicial.txt";
+        String arquivoPalavrasChave = "PalavrasChave.txt";
+        String arquivoTexto = "textoInicial.txt";
         String arquivoSaida = "indice_remissivo_saida.txt";
 
 
